0357f71015277cde8a951ec26166a7f0b1521082
ferrnavarro <fer.maganavarro@gmail.com>
2019-09-24 22:47:37 -0600

    Showing table of restaurants and search by name

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Fernando's Pizza", Location = "San Salvador", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 2, Name = "El taco loco", Location = "Nexico City", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id = 3, Name = "Santaburguesa", Location = "San Salvador", Cuisine = CuisineType.None }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+
+}
